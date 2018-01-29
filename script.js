fc62606dfd544d22aab3419378879b79ea87784e
Jazzzter <zadorozhniy.igor@gmail.com>
2018-01-29 17:18:30 +0300

    revision A

---
+++ script.js
@@ -0,0 +1,136 @@
+// var game = new Array(4);
+// for (var i = 0; i <= 3; i++) {
+//     game[i] = new Array(4);
+//     for (var j = 0; j <= 3; j++) {
+//         game[i][j] = 0;
+//     }
+// }
+
+var i, j, k, n, game;
+
+function newGame() {        // Начало новой игры 
+    game = [[0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]];     // Заполняем массив нулевыми значениями
+
+    addElements(game, 4);   // Добавляем 4 элемента
+    showGame(game);         // Выводим результат на екран
+    return game;            
+}
+
+function moveUp(game) {
+    if (game) { // Если масив заполнен элементами
+        for (j = 0; j <= 3; j++) {
+            
+            // Сдвигаем элементы массива вверх
+            for (k = 0; k <= 3; k++) {
+                if (!game[0][j]) {
+                    game[0][j] = game[1][j];
+                    game[1][j] = game[2][j];
+                    game[2][j] = game[3][j];
+                    game[3][j] = 0;
+                } 
+                else if (!game[1][j]) {
+                    game[1][j] = game[2][j];
+                    game[2][j] = game[3][j];
+                    game[3][j] = 0;
+                }
+                else if (!game[2][j]) {
+                    game[2][j] = game[3][j];
+                    game[3][j] = 0;
+                }
+            }
+
+            // Суммируем одинаковые соседние элементы
+            if (game[0][j] === game[1][j]) {
+                game[0][j] += game[1][j];
+                game[1][j] = game[2][j];
+                game[2][j] = game[3][j];
+                game[3][j] = 0;
+            } 
+            else if (game[1][j] === game[2][j]) {
+                game[1][j] += game[2][j];
+                game[2][j] = game[3][j];
+                game[3][j] = 0;
+            }
+            else if (game[2][j] === game[3][j]) {
+                game[2][j] += game[3][j];
+                game[3][j] = 0;
+            }
+        }
+        addElements(game, 2);   // Добавляем 2 элемента
+        showGame(game);         // Выводим результат на екран
+        return game;
+    } else alert('Press New Game') // Если масив не заполнен элементами (не нажата кнопка New Game) выводим сообщение
+}
+
+function moveDown(game) {
+    if (game) { // Если масив заполнен элементами
+        for (j = 3; j >= 0; j--) {
+            
+            // Сдвигаем элементы массива вниз
+            for (k = 0; k <= 3; k++) {
+                if (!game[3][j]) {
+                    game[3][j] = game[2][j];
+                    game[2][j] = game[1][j];
+                    game[1][j] = game[0][j];
+                    game[0][j] = 0;
+                } 
+                else if (!game[2][j]) {
+                    game[2][j] = game[1][j];
+                    game[1][j] = game[0][j];
+                    game[0][j] = 0;
+                }
+                else if (!game[1][j]) {
+                    game[1][j] = game[0][j];
+                    game[3][j] = 0;
+                }
+            }
+
+            // Суммируем одинаковые соседние элементы
+            if (game[3][j] === game[2][j]) {
+                game[3][j] += game[2][j];
+                game[2][j] = game[1][j];
+                game[1][j] = game[0][j];
+                game[0][j] = 0;
+            } 
+            else if (game[2][j] === game[1][j]) {
+                game[2][j] += game[1][j];
+                game[1][j] = game[0][j];
+                game[0][j] = 0;
+            }
+            else if (game[1][j] === game[0][j]) {
+                game[1][j] += game[0][j];
+                game[0][j] = 0;
+            }
+        }
+        addElements(game, 2);
+        showGame(game);
+        return game;
+    } else alert('Press New Game') // Если масив не заполнен элементами (не нажата кнопка New Game) выводим сообщение
+}
+
+
+
+function addElements(game, n) { // Функция добавления элементов
+    for (k = 0; k <= n - 1; k++) {
+        var x = Math.round(Math.random() * 3);
+        var y = Math.round(Math.random() * 3);
+
+        if (game[x][y]) {
+            k = k - 1;
+        } else {
+            game[x][y] = Math.random() < 0.5 ? 2 : 4;
+        }
+    }
+}
+
+function showGame(game) { // Функция вывода массива на экран
+    for (i = 0; i <= 3; i++) {
+        for (j = 0; j <= 3; j++) {
+            document.getElementById(i + '-' + j).innerHTML = game[i][j];
+        }
+    }
+}
+
